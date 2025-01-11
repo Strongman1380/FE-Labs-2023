db87df9272c438ba6a8792c7ccfde18c02322109
Stongman1380 <bhinrichs1380@gmail.com>
2025-01-11 13:08:38 -0600

    Complete Lab04 JavaScript variables and operations exercise

--- Lab04/JS1VariablesAndOperations.js
+++ Lab04/JS1VariablesAndOperations.js
@@ -37,82 +37,43 @@
 
 console.log(`--------------------------
 Question 1. Creating & 'console.log()'ing variables \n`);
-
-/**
- * Instruction: Create a variable for each step, then print that variable 
- *              to the console in the correct section.
- *
- * Step 1: Create a variable to hold the quantity of available seats left on a flight.
- * Step 2: Create a variable to hold the cost of groceries at checkout
- * Step 3: Create a variable to hold a person's middle initial
- * Step 4: Is it Hot Outside?
- *         Create a variable to hold a boolean, true if it's hot outside, false if it's not hot outside.
- * Step 5: Create a variable to hold a customer's first name.
- * Step 6: Create a variable to hold a street address.
- * ↓ YOUR CODE HERE ↓ */
-
 console.log("Step 1:  Number of Seats on a Plane");
-
-
 console.log("Step 2:  Cost of Groceries");
-
-
 console.log("Step 3:  Middle Initial");
-
-
 console.log("Step 4:  Is it Hot Outside?");
-
-
 console.log("Step 5:  First Name");
-
-
 console.log("Step 6:  Street Address");
-
-
-/*-------------------------------------------------------*/
-
 console.log(`--------------------------
 Question 2. Creating & 'console.log()'ing variables \n`);
  
-
-/** 
- * Instruction: Use the variables we just created to solve the problems below.
- *              Print your answers to the console.
- * 
- *
- * */
-
     console.log('Remove Plane Seats:')
-//	A customer booked 2 plane seats. Remove 2 seats from the available seats variable
-//  ↓ YOUR CODE HERE ↓
 
+let seats= 10;
+let bookedSeats = 2;
 
     console.log('Buy a Candy Bar:')
-//	Impulse candy bar purchase! Add 2.15 to the grocery total
-//  ↓ YOUR CODE HERE ↓
 
+let groceriesTotal = 20;
+let candyBarCost = 2.15;
 
     console.log('Fix a Birth Certificate:')
-//	A birth certificate was printed incorrectly, change the middle initial to something else
-//  ↓ YOUR CODE HERE ↓
 
+let birthCertificate = 'M';
+let newBirthCertificate = 'S';
 
     console.log('Changing Seasons:')
-//	The season has changed, update the hot outside variable	to be opposite of what it was
-//  ↓ YOUR CODE HERE ↓
-    
+
+Let hotOutside = true;
+let newSeason =!hotOutside;
 
     console.log('Full Name:')
-//	Create a new variable called full name using the customer's
-//  first name, the middle initial, and a last name of your choice
-//  ↓ YOUR CODE HERE ↓
-
+var firstName = 'John';
+var middleInitial = 'S';    
+var lastName = 'Dotes';
 
     console.log('Introduce a Customer:')
-//	Print a line to the console that introduces the customer 
-//	including the name, and saying that they live at the address variable
-//  ↓ YOUR CODE HERE ↓
-
+var address = '123 Main St';
+var introduction = 'Hello, my name is ' + firstName + ' ' + middleInitial + ' ' + lastName + ' and I live at ' + address;
 
 
     console.log('-----------Finished------------')
